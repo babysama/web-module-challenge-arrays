366b498789dfaf6fd969ea239fd4b417730c465f
KuikuiBaba Li <kuikuibabali@KuikuiBabas-MBP.home>
2021-11-03 19:57:46 -0700

    sloved

--- index.js
+++ index.js
@@ -45,8 +45,15 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(arr){
+  let copyArr = [];
+  copyArr.length = arr.length;
+  for (let i = 0; i < arr.length; i++) {
+    let curEle = arr[i];
+    copyArr[i] = curEle;
+  }
+
+  return copyArr;
 }    
 
 
@@ -64,8 +71,8 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
+function is31Flavors(arr){
+  return arr.length === 31;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -81,8 +88,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(arr, newFlavor){
+  arr.unshift(newFlavor);
+  return arr;
 }
 
 
@@ -97,8 +105,9 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(arr){
+  arr.pop();
+  return arr;
 }
 
 
@@ -114,8 +123,8 @@ Use the getFlavorByIndex function below to do the following:
   For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+function getFlavorByIndex(arr, index){
+  return arr[index];
 }
 
 
@@ -134,8 +143,15 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+function removeFlavorByName(arr, flavor){
+  let flavorIndex = arr.indexOf(flavor);
+  // console.log(flavorIndex)
+  let frontArr = arr.slice(0, flavorIndex);
+  // console.log(frontArr);
+  let backArr = arr.slice(flavorIndex + 1, arr.length);
+  // console.log(backArr);
+  frontArr.push(...backArr);
+  return frontArr;
 }
 
 
@@ -160,8 +176,16 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(arr, str){
+  let resArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let curEle = arr[i];
+    if (curEle.includes(str)) {
+      resArr.push(curEle);
+    }
+  }
+
+  return resArr;
 }
 
 
